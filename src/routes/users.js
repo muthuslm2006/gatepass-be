09fcc2db7149c2c713c17fbe5d860a7db4bb1ea7
muthuslm2006 <98971383+muthuslm2006@users.noreply.github.com>
2024-12-20 16:10:42 +0530

    some package instll

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/test',function(req,res){
+  res.send("Welcome");
+  res.end();
+})
+
 module.exports = router;
